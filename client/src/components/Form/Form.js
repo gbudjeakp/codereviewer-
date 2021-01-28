eade38c770d873b6c4a905ba8a3e20106b136386
Akpevwen Gbudje <gbudjeakp@gmail.com>
2021-01-28 14:42:21 -0600

    setting up redux

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { TextField, Button, Typography, Paper } from '@material-ui/core';
-import useStyles from './styles';
+import { TextField, Button, Typography, Paper } from '@material-ui/core'
+import useStyles from './styles'
 
-function Form() {
-    return (
-        <div>
-           
-        </div>
-    )
+function Form () {
+  return (
+    <div>
+          <h1>Form</h1> 
+    </div>
+  )
 }
 
 export default Form

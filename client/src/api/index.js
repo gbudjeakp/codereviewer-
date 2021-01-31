0405d81ad5f8e395e608d4bc2db5dc05fc29f679
Akpevwen Gbudje <gbudjeakp@gmail.com>
2021-01-31 16:51:12 -0600

    mobile fix

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({ baseURL: process.env.REACT_APP_SERVER_API })
-
+// process.env.REACT_APP_SERVER_API
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
     req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
